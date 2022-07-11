7a66bbe43ae521f713fcecdbd264c6ab11787c55
Carlos de Lannoy <carlos.delannoy@wur.nl>
2022-07-11 17:11:26 +0200

    fix conda recipe

--- setup.py
+++ setup.py
@@ -8,27 +8,27 @@ setup(
     name='baseLess',
     version='0.0.1',
     packages=['baseLess'],
-    # install_requires=[
-    #     'matplotlib==3.5.1',  # avoid issues with heatmaps
-    #     'Cython==0.29.30',
-    #     'tensorflow-gpu==2.8.1',
-    #     'pandas==1.4.2',
-    #     'bokeh==2.4.3',
-    #     'snakemake==7.8.1',
-    #     'h5py==3.6.0',
-    #     'zodb==5.7.0',
-    #     'hyperopt==0.2.7',
-    #     'seaborn==0.11.2',
-    #     'numpy==1.22.4',
-    #     'tables==3.7.0',
-    #     'scikit-learn==1.1.1',
-    #     'fastparquet==0.8.1'
-    #                   ],
+    install_requires=[
+        'matplotlib==3.5.1',  # avoid issues with heatmaps
+        'Cython==0.29.30',
+        'tensorflow-gpu==2.8.1',
+        'pandas==1.4.2',
+        'bokeh==2.4.3',
+        'snakemake==7.8.1',
+        'h5py==3.6.0',
+        'zodb==5.7.0',
+        'hyperopt==0.2.7',
+        'seaborn==0.11.2',
+        'numpy==1.22.4',
+        'tables==3.7.0',
+        'scikit-learn==1.1.1',
+        'fastparquet==0.8.1'
+                      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.10',
     author='Carlos de Lannoy',
     author_email='cvdelannoy@gmail.com',
     description='Detect sequences in MinION squiggles, no basecalling required',
