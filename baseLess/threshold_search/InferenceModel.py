244844052aed63732ab001f33695cfb591fec8d7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 15:25:56 +0000

    Adding tarfile member sanitization to extractall()

--- baseLess/threshold_search/InferenceModel.py
+++ baseLess/threshold_search/InferenceModel.py
@@ -35,7 +35,29 @@ class InferenceModel(object):
         """
         with TemporaryDirectory() as td:
             with tarfile.open(mod_fn) as fh:
-                fh.extractall(td)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(fh, td)
             out_dict = {}
 
             def loss_fun(y_true, y_pred):  # just dummy to satisfy the stupid thing being there
